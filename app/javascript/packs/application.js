2f3288904b14e7958d02d5fdb79f3aa23516038d
pauline rouger <paulinerouge@hotmail.com>
2022-01-17 18:59:32 +0800

    .gitignore

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("stylesheets/application.scss")
+import 'stylesheets/application.scss'
+
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -26,6 +28,7 @@ require("stylesheets/application.scss")
 import "bootstrap";
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
+
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
